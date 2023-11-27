26b86b2abec30cbd7d759f690a1395c743f51bcb
Welly Mayara <welly.maya.s@gmail.com>
2023-11-26 21:17:31 -0300

    test: ensure password have at least 8 characters

---
+++ src/service/securePassword.spec.ts
@@ -0,0 +1,12 @@
+import Errors from '../enum/erros';
+import SecurePasswordService from './securePassword.service';
+
+describe('Secure Password Service Test', () => {
+  const sut = new SecurePasswordService();
+
+  it('Should return error if the length of the password is less than 9 characters', () => {
+    const password = '';
+    const errorMessage = sut.verifyPassword(password);
+    expect(errorMessage).toContain(Errors.MIN_ERROR);
+  });
+});
